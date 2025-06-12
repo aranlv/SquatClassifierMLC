0fdd29044e97ba33312d56729213766c9e5d2007
Hafizh4321 <muhammadhafizh4321@gmail.com>
2025-06-12 11:21:22 +0700

    test ubah camera jadi cameraaa

---
+++ SquatClassifierMLC/App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SquatClassifierMLC
+//
+//  Created by Aretha Natalova Wahyudi on 12/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CameraWithPosesAndOverlaysView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SquatClassifierMLC/App/SquatClassifierMLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquatClassifierMLCApp.swift
+//  SquatClassifierMLC
+//
+//  Created by Aretha Natalova Wahyudi on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SquatClassifierMLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
