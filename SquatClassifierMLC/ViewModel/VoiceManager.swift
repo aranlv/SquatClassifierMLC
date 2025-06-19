6e3031056e18472ca3503f6170d859156f2ed871
aretha <aretha.wahyudi@binus.ac.id>
2025-06-19 09:40:59 +0700

    voice feedback main thread

--- SquatClassifierMLC/ViewModel/VoiceManager.swift
+++ SquatClassifierMLC/ViewModel/VoiceManager.swift
@@ -9,18 +9,21 @@ import Foundation
 import AVFoundation
 import Speech
 
+@MainActor
 class VoiceCommandManager: NSObject, ObservableObject, SFSpeechRecognizerDelegate, AVSpeechSynthesizerDelegate {
     
+    // MARK: - Voice Synthesizer
+    private let speechSynthesizer = AVSpeechSynthesizer()
+
     // MARK: - Published Properties
-    @Published var isWorkoutRunning: Bool = false
-    @Published var lastCommand: String = ""
-    @Published var isListening: Bool = false
+//    @Published var isWorkoutRunning: Bool = false
+//    @Published var lastCommand: String = ""
+//    @Published var isListening: Bool = false
     
 //    // MARK: - Private Properties
 //    private var lastRecognizedPhrase: String = ""
 //    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
 //    private let audioEngine = AVAudioEngine()
-    private let speechSynthesizer = AVSpeechSynthesizer()
 //    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
 //    private var recognitionTask: SFSpeechRecognitionTask?
 
@@ -32,26 +35,32 @@ class VoiceCommandManager: NSObject, ObservableObject, SFSpeechRecognizerDelegat
 
     // MARK: - Speech
     func speakFeedback(number: Int, label: String) {
-        let formattedLabel = formatLabelForSpeech(label)
-        let text = String("Rep \(number), \(formattedLabel)")
-        let utterance = AVSpeechUtterance(string: text)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-        utterance.rate = 0.5
-        speechSynthesizer.speak(utterance)
+        DispatchQueue.main.async {
+            let formattedLabel = self.formatLabelForSpeech(label)
+            let text = String("Rep \(number), \(formattedLabel)")
+            let utterance = AVSpeechUtterance(string: text)
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+            utterance.rate = 0.5
+            self.speechSynthesizer.speak(utterance)
+        }
     }
 
     func speakCountdown(number: Int) {
-        let utterance = AVSpeechUtterance(string: "\(number)")
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-        utterance.rate = 0.5
-        speechSynthesizer.speak(utterance)
+        DispatchQueue.main.async {
+            let utterance = AVSpeechUtterance(string: "\(number)")
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+            utterance.rate = 0.5
+            self.speechSynthesizer.speak(utterance)
+        }
     }
 
     func speakStartWorkout() {
-        let utterance = AVSpeechUtterance(string: "GO")
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-        utterance.rate = 0.6
-        speechSynthesizer.speak(utterance)
+        DispatchQueue.main.async {
+            let utterance = AVSpeechUtterance(string: "GO")
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+            utterance.rate = 0.6
+            self.speechSynthesizer.speak(utterance)
+        }
     }
     
     private func formatLabelForSpeech(_ label: String) -> String {
